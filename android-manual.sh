123f0ef3d8415d2708317465288360eaddb1daa9
passcombo <35246839+passcombo@users.noreply.github.com>
2020-02-15 12:53:51 +0100

    Create android-manual.sh

---
+++ android-manual.sh
@@ -0,0 +1,34 @@
+# This app works in terminal as user interface - Termux app is required to use on Android
+
+# Environment Setup:
+# 1. install Termux app
+# 2. Open Termux and run following commands:
+#    - "pkg install wget" # [must have] to donwload scripts
+#    - "pkg install gnupg" # [must have] for encryptions
+#    - "pkg install python" # [must have] for app to run
+#    - "pkg install pip" # [must have] for app to install additional module
+#    - "pip install psutil" # [must have] for app additional library
+
+# Download app [here example second version]:
+# "wget https://github.com/passcombo/gpgon/archive/2nd.tar.gz"
+
+# Extract:
+# "tar xfvz 2nd.tar.gz"
+
+# See intro.pdf for mailbox config
+# Every Android user shuld have gmail box that works with the app
+# Just need to configure 2FA and app password in google account setup
+
+# Run app:
+# "cd gpgon-2nd"
+# "python gpgon.py"
+
+# Optional:
+#    - "pkg install nano" # [optional] useful easy text editor
+#    - "termux-setup-storage" # [optional] for external storage access - see termux manual
+
+# [ERRORS?] If psutil python library won't install try this:
+#    - "apt-get update --fix-missing" 
+#    - "pkg install binutils"
+#    - "pkg install clang"
+#    - "pip install psutil"
